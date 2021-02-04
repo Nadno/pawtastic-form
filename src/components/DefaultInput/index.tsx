8d9c083a586a2296c170451e5b460e99761db31e
Nadno <eunadno@gmail.com>
2021-02-04 15:25:05 -0300

    inputText changed to default input

---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,38 @@
+import React, { ChangeEvent } from 'react';
+import './index.scss';
+
+interface Props {
+  id: string;
+  name: string;
+  label: string;
+  error?: string;
+  handleChange(e: ChangeEvent): void;
+  [propName: string]: any;
+}
+
+const DefaultInput = ({
+  id,
+  name,
+  label,
+  error,
+  handleChange,
+  ...props
+}: Props) => {
+  return (
+    <div className={`default-input ${error && 'error'}`}>
+      <label htmlFor={id} className="input-text__title">
+        {label}
+      </label>
+      <input
+        type="text"
+        id={id}
+        name={name}
+        onChange={handleChange}
+        {...props}
+      />
+      {error && <span className="error-field">{error}</span>}
+    </div>
+  );
+};
+
+export default DefaultInput;
