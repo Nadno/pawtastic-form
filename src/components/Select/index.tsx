d5e1bc9abff6750a0ee3cde8b434244314ab9575
Nadno <eunadno@gmail.com>
2021-02-03 22:04:33 -0300

    adjustments on Select and Select option

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import './index.scss';
 
 interface Props {
   id: string;
+  title: string;
+  error: string;
   children: any;
   columns: 2 | 4;
 }
 
-const Select = ({ id, children, columns }: Props) => {
+const Select = ({ id, title, error, children, columns }: Props) => {
   const columnsOp = new Map();
   columnsOp.set(2, 'two');
   columnsOp.set(4, 'four');
 
   return (
-    <div className={`select ${columnsOp.get(columns)}-columns`} id={id}>
-      {children}
+    <div className="select-field" >
+      <span className="select-field__title">{title}</span>
+
+      <div className={
+        `select ${error && 'error'} ${columnsOp.get(columns)}-columns`
+        }
+        id={id}
+      >
+        {children}
+      </div>
+      <span className="error-field">{error}</span>
     </div>
   );
 };
 
 interface OptionProps {
-  id: string;
+  id?: string;
   name: string;
   label: string;
-  children: any;
+  children?: any;
+  handleChange(e: FormEvent): void;
 }
 
-Select.Option = ({ id, name, label, children }: OptionProps) => {
+Select.Option = ({ id, name, label, children, handleChange }: OptionProps) => {
   return (
     <>
-      <input type="radio" name={name} id={id} value="dog" />
+      <input type="radio" name={name} id={id} value={id} onChange={handleChange} />
       <label htmlFor={id} className="option__legend">
         {children}
         <span>{label}</span>
