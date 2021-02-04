4fd342351f3ebcdd6ab4145f2d6629c5a3cdb28e
Nadno <eunadno@gmail.com>
2021-02-03 21:58:03 -0300

    added zipCode

---
+++ src/interfaces/form.d.ts
@@ -0,0 +1,31 @@
+export interface MyFormData {
+  zipCode: string;
+  email?: string;
+  password?: string;
+  confirm?: string;
+  policy?: boolean;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+  altPhone?: string;
+  cpf?: string;
+  petType?: string;
+  petName?: string;
+  petBreed?: string;
+  petGender?: string;
+  petBirthDay?: string;
+  petSpayedOrNeutered?: string;
+  petWeight?: string;
+  petPhoto?: string;
+
+  kisses?: boolean;
+  walk?: boolean;
+  barking?: boolean;
+  snuggling?: boolean;
+  treats?: boolean;
+  playingFetch?: boolean;
+  naps?: boolean;
+  toys?: boolean;
+
+  petDetail?: string;
+}
