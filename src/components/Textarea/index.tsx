f2ca3b18e3f61b0266926341e0864156541719fd
Nadno <eunadno@gmail.com>
2021-02-06 15:45:41 -0300

    added custominput interface

---
+++ src/components/Textarea/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CustomInputProps from '../../types/inputs'
+;
+import './index.scss';
+
+interface Props extends CustomInputProps {
+  maxLength: number;
+}
+
+const Textarea = ({ label, handleChange, ...props }: Props) => {
+  return (
+    <div className="text-field">
+      <label htmlFor="pet-detail" className="text-field__title">
+        {label} Something that we must know?
+      </label>
+      <textarea onChange={handleChange} {...props}></textarea>
+    </div>
+  );
+};
+
+export default Textarea;
