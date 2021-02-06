75eb8ff520b214dab47a3b7d8d1fb55e55ad9be4
Nadno <eunadno@gmail.com>
2021-02-06 15:11:29 -0300

    added background state to LeftSide

--- src/layouts/LeftSide/index.tsx
+++ src/layouts/LeftSide/index.tsx
@@ -1,12 +1,4 @@
-import React from 'react';
-
-import firstBg from '../../assets/images/sign-up__0.jpg';
-import secondBg from '../../assets/images/sign-up__1.jpg';
-import thirdBg from '../../assets/images/sign-up__2.jpg';
-import fourthBg from '../../assets/images/sign-up__3.jpg';
-import fifthBg from '../../assets/images/sign-up__4.jpg';
-import sixthBg from '../../assets/images/sign-up__5.jpg';
-import seventhBg from '../../assets/images/sign-up__5.jpg';
+import React, { useEffect, useState } from 'react';
 
 import { useStep } from '../../contexts/useStep';
 
@@ -17,19 +9,17 @@ interface Props {
 
 const LeftSide = ({ children }: Props) => {
   const { step } = useStep(0, 6);
-  const backgrounds = [
-    firstBg,
-    secondBg,
-    thirdBg,
-    fourthBg,
-    fifthBg,
-    sixthBg,
-    seventhBg,
-  ];
-  const backgroundImage = `url(${backgrounds[step]})`;
+  const [img, setImg] = useState('');
+
+  useEffect(() => {
+    import(`../../assets/images/sign-up__${step}.jpg`)
+      .then(imgModule => {
+        setImg(`url(${imgModule.default})`)
+      });
+  }, [step]);
 
   return (
-    <div className="side" style={{ backgroundImage }}>
+    <div className="side" style={{ backgroundImage: img }}>
       {children}
     </div>
   );
