7a56b81ea9cedb2f8c554b70b935404e51b0dd8b
Nadno <eunadno@gmail.com>
2021-02-06 10:21:45 -0300

    created ErrorBoundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  state: { hasError: boolean; errorMessage: string } = {
+    hasError: false,
+    errorMessage: '',
+  };
+
+  static getDerivedStateFromError(err: Error) {
+    return {
+      hasError: true,
+      errorMessage: err,
+    };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      console.error(this.state.errorMessage);
+      return (
+        <div className="error">
+          <p>
+            Ocorreu um error inesperado, por favor reinicie a página ou tente
+            mais tarde.
+          </p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
